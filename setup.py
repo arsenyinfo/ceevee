01e3c6ac7eef46fb910c8e3fe5238609a2fa842c
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2019-09-10 21:33:10 +0300

    * update setup.py * remove trailing whitespace from readme

--- setup.py
+++ setup.py
@@ -1,18 +1,110 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the "upload" functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = "ceevee"
+DESCRIPTION = "Deep learning based baselines for various problems"
+URL = "https://github.com/arsenyinfo/ceevee"
+AUTHOR = "[ods.ai] Arseny Kravchenko"
+REQUIRES_PYTHON = ">=3.6.0"
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-def parse_requirements(filename):
-    lineiter = (line.strip() for line in open(filename))
+def load_requirements(filename):
+    with open(os.path.join(PROJECT_ROOT, filename), "r") as f:
+        lineiter = f.read().splitlines()
+
     return [line for line in lineiter if line and not line.startswith("#")]
 
 
+def load_readme():
+    readme_path = os.path.join(PROJECT_ROOT, "readme.md")
+    with io.open(readme_path, encoding="utf-8") as f:
+        return "\n" + f.read()
+
+
+def load_version():
+    context = {}
+    with open(os.path.join(PROJECT_ROOT, "ceevee", "__version__.py")) as f:
+        exec(f.read(), context)
+    return context["__version__"]
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(PROJECT_ROOT, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(
+            "{0} setup.py sdist bdist_wheel --universal".format(sys.executable)
+        )
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(load_version()))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
 setup(
-    name="ceevee",
-    version="0.0.1",
-    author="[ods.ai] Arseny Kravchenko",
-    description="Deep learning based baselines for various problems",
-    package_dir={'': './'},
-    packages=['ceevee'],
+    name=NAME,
+    version=load_version(),
+    description=DESCRIPTION,
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    keywords=[
+        "Machine Learning",
+        "Deep Learning",
+        "Computer Vision",
+        "PyTorch",
+    ],
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=load_requirements("requirements.txt"),
     include_package_data=True,
-    install_requires=parse_requirements("requirements.txt")
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
